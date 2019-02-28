f4297bd33345354f4a4b26dee1fefc2fe19ab386
Caique Lima <caique.lima@nubank.com.br>
2019-02-28 20:13:26 -0300

    Add python package related code

---
+++ src/fklearn/version.py
@@ -0,0 +1,16 @@
+from os.path import dirname, join
+
+
+def version():
+    """
+    Get package version
+
+    Returns
+    -------
+    version : str
+    """
+    with open(join(dirname(__file__), 'resources', 'VERSION')) as f:
+        return f.read().strip()
+
+
+__version__ = version()
