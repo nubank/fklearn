d6e166e03af16d20f6d112c703ec1ad58b3d8de1
Sadi Kneipp <sadi.kneipp@nubank.com.br>
2019-04-16 14:16:47 -0300

    extends nullify, compacts perturbator

--- src/fklearn/validation/perturbators.py
+++ src/fklearn/validation/perturbators.py
@@ -3,34 +3,38 @@ import random
 from toolz import curry
 import pandas as pd
 from typing import Callable, List
+from fklearn.types import ColumnWisePerturbFnType
 
 @curry
-def shift_mu (col: pd.Series, perc: float) -> pd.Series:
+def shift_mu(col: pd.Series, perc: float) -> pd.Series:
     mu = np.mean(col)
     col = col + mu * perc
     return col
 
 @curry
-def random_noise (col: pd.Series) -> pd.Series:
+def random_noise(col: pd.Series) -> pd.Series:
     mu = np.mean(col)
     std = np.std(col)
     noise = np.random.normal(mu, std, len(col))
     return col + noise
 
 @curry
-def nullify(col: pd.Series) -> pd.Series:
-    return pd.Series([np.nan] * len(col))
+def nullify(col: pd.Series, perc: float = 1) -> pd.Series:
+    # default behavior to nullify whole column
+    n = len(col)
+    ix_to_nan = random.sample(range(n), int(n * perc))
+    ret = col.copy(deep=True)
+    ret.iloc[ix_to_nan] = np.nan
+    return ret
   
 @curry
-def sample_columns (data: pd.DataFrame, perc: float) -> List[str]:
+def sample_columns(data: pd.DataFrame, perc: float) -> List[str]:
     return random.sample(list(data.columns), int(len(data.columns) * perc))
 
 @curry
-def perturbator(data: pd.DataFrame, cols: List[str], corr_fn: Callable) -> pd.DataFrame:
-    copy = data.copy(deep=True)
-    for col in cols:
-        copy.loc[:, col] = corr_fn(data[col])
-    return copy 
+def perturbator(data: pd.DataFrame,
+                cols: List[str],
+                corruption_fn: ColumnWisePerturbFnType) -> pd.DataFrame:
     """
     transforms specific columns of a dataset according to an artificial
     corruption function.
@@ -43,10 +47,11 @@ def perturbator(data: pd.DataFrame, cols: List[str], corr_fn: Callable) -> pd.Da
     cols : List[str]
         A list of columns to apply the corruption function
 
-    corr_fn : function pandas.Series -> pandas.Series
+    corruption_fn : function pandas.Series -> pandas.Series
         An arbitrary corruption function
 
     Returns
     ----------
-    A list of log-like dictionary evaluations.
+    A transformed dataset
     """
+    return data.assign(**{col: corruption_fn(col) for col in cols}) 
