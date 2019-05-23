21abdb85f39c076c3494fb55815706156dcc5aaa
Jessica de Sousa <jessica.desousa@nubank.com.br>
2019-05-23 15:39:44 -0300

    Changing the nan column to not include the first one dropped

--- src/fklearn/training/transformation.py
+++ src/fklearn/training/transformation.py
@@ -690,12 +690,12 @@ def onehot_categorizer(df: pd.DataFrame,
         Whether to store the feature value -> integer dictionary in the log
     """
 
-    categ_getter = lambda col: list(np.sort(df[col].dropna(axis=0, how='any').unique())[int(drop_first_column):])
+    categ_getter = lambda col: list(np.sort(df[col].dropna(axis=0, how='any').unique()))
     vec = {column: categ_getter(column) for column in sorted(columns_to_categorize)}
 
     def p(new_df: pd.DataFrame) -> pd.DataFrame:
         make_dummies = lambda col: dict(map(lambda categ: (col + "==" + str(categ), (new_df[col] == categ).astype(int)),
-                                            vec[col]))
+                                            vec[col][int(drop_first_column):]))
 
         oh_cols = dict(mapcat(lambda col: merge(make_dummies(col),
                                                 {col + "==nan": (~new_df[col].isin(vec[col])).astype(
