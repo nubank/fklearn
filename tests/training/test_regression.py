58d30ae5c7266ab56e3dceae2be28559cf0cb1e0
vultor33 <bastardo_220@hotmail.com>
2019-07-25 15:33:57 -0300

    adding custom model to pipeline (issue #70) (#77)
    * adding custom model to pipeline (issue #70) - first commit
    
    * using PEP8 style
    
    * typo on predict and predict_proba error message

--- tests/training/test_regression.py
+++ tests/training/test_regression.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 from fklearn.training.regression import \
     linear_regression_learner, gp_regression_learner, \
-    xgb_regression_learner, lgbm_regression_learner, catboost_regressor_learner
+    xgb_regression_learner, lgbm_regression_learner, catboost_regressor_learner, \
+    custom_supervised_model_learner
 
 
 def test_linear_regression_learner():
@@ -221,3 +222,68 @@ def test_catboost_regressor_learner():
     assert "shap_values" in pred_shap.columns
     assert "shap_expected_value" in pred_shap.columns
     assert np.vstack(pred_shap["shap_values"]).shape == (4, 2)
+
+
+def test_custom_supervised_model_learner():
+    from sklearn import tree
+    from sklearn.gaussian_process import GaussianProcessRegressor
+
+    df_train_classification = pd.DataFrame({
+        'id': ["id1", "id2", "id3", "id4"],
+        'x1': [10.0, 13.0, 10.0, 13.0],
+        "x2": [0, 1, 1, 0],
+        'y': [0, 1, 0, 1]
+    })
+    df_test_classification = pd.DataFrame({
+        'id': ["id4", "id4", "id5", "id6"],
+        'x1': [12.0, 1000.0, -4.0, 0.0],
+        "x2": [1, 1, 0, 1],
+        'y': [1, 0, 0, 1]
+    })
+    df_train_regression = pd.DataFrame({
+        'id': ["id1", "id2", "id3", "id4"],
+        'x1': [10.0, 13.0, 10.0, 13.0],
+        "x2": [0, 1, 1, 0],
+        'y': [2.3, 4.0, 100.0, -3.9]
+    })
+    df_test_regression = pd.DataFrame({
+        'id': ["id4", "id4", "id5", "id6"],
+        'x1': [12.0, 1000.0, -4.0, 0.0],
+        "x2": [1, 1, 0, 1],
+        'y': [1.3, -4.0, 0.0, 49]
+    })
+
+    features = ["x1", "x2"]
+    custom_classifier_learner = custom_supervised_model_learner(
+        features=features,
+        target='y',
+        model=tree.DecisionTreeClassifier(),
+        supervised_type='classification',
+        log={'DecisionTreeClassifier': None})
+    predict_fn_classification, pred_train_classification, log = custom_classifier_learner(df_train_classification)
+    pred_test_classification = predict_fn_classification(df_test_classification)
+
+    expected_col_train = df_train_classification.columns.tolist() + ["prediction_0", "prediction_1"]
+    expected_col_test = df_test_classification.columns.tolist() + ["prediction_0", "prediction_1"]
+    assert(Counter(expected_col_train) == Counter(pred_train_classification.columns.tolist()))
+    assert(Counter(expected_col_test) == Counter(pred_test_classification.columns.tolist()))
+    assert(pred_test_classification.prediction_0.max() <= 1)
+    assert(pred_test_classification.prediction_0.min() >= 0)
+    assert(pred_test_classification.prediction_1.max() <= 1)
+    assert(pred_test_classification.prediction_1.min() >= 0)
+
+    custom_regression_learner = custom_supervised_model_learner(
+        features=features,
+        target='y',
+        model=GaussianProcessRegressor(),
+        supervised_type='regression',
+        log={'GaussianProcessRegressor': None})
+    predict_fn, pred_train, log = custom_regression_learner(df_train_regression)
+    pred_test = predict_fn(df_test_regression)
+
+    expected_col_train = df_train_regression.columns.tolist() + ["prediction"]
+    expected_col_test = df_test_regression.columns.tolist() + ["prediction"]
+    assert(Counter(expected_col_train) == Counter(pred_train.columns.tolist()))
+    assert(Counter(expected_col_test) == Counter(pred_test.columns.tolist()))
+    assert((pred_test.columns == pred_train.columns).all())
+    assert("prediction" in pred_test.columns)
