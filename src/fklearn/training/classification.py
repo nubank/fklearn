ffcc1ea67e17c357c3754e832d2d3f45e215669b
Tatyana Zabanova <tatyana.zabanova@nubank.com.br>
2020-01-14 17:57:10 -0300

    fix catboost shap compatibility

--- src/fklearn/training/classification.py
+++ src/fklearn/training/classification.py
@@ -244,18 +244,23 @@ def catboost_classification_learner(df: pd.DataFrame,
     with the specified features and labels from `df`. Then, it fits a CatBoost
     model to this DMatrix. Return the predict function for the model and the
     predictions for the input dataset.
+
     Parameters
     ----------
+
     df : pandas.DataFrame
         A Pandas' DataFrame with features and target columns.
         The model will be trained to predict the target column
         from the features.
+
     features : list of str
         A list os column names that are used as features for the model. All this names
         should be in `df`.
+
     target : str
         The name of the column in `df` that should be used as target for the model.
         This column should be discrete, since this is a classification model.
+
     learning_rate : float
         Float in the range (0, 1]
         Step size shrinkage used in update to prevents overfitting. After each boosting step,
@@ -263,21 +268,26 @@ def catboost_classification_learner(df: pd.DataFrame,
         feature weights to make the boosting process more conservative.
         See the eta hyper-parameter in:
         https://catboost.ai/docs/concepts/python-reference_parameters-list.html
+
     num_estimators : int
         Int in the range (0, inf)
         Number of boosted trees to fit.
         See the n_estimators hyper-parameter in:
         https://catboost.ai/docs/concepts/python-reference_parameters-list.html
+
     extra_params : dict, optional
         Dictionary in the format {"hyperparameter_name" : hyperparameter_value}.
         Other parameters for the CatBoost model. See the list in:
         https://catboost.ai/docs/concepts/python-reference_catboostregressor.html
         If not passed, the default will be used.
+
     prediction_column : str
         The name of the column with the predictions from the model.
         If a multiclass problem, additional prediction_column_i columns will be added for i in range(0,n_classes).
+
     weight_column : str, optional
         The name of the column with scores to weight the data.
+
     encode_extra_cols : bool (default: True)
         If True, treats all columns in `df` with name pattern fklearn_feat__col==val` as feature columns.
     """
