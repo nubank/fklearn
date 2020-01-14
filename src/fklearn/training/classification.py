f2191bb1ab4f8a1a0eb002b5fd31a2a0cb903f1e
Tatyana Zabanova <tatyana.zabanova@nubank.com.br>
2020-01-14 18:07:35 -0300

    reformat

--- src/fklearn/training/classification.py
+++ src/fklearn/training/classification.py
@@ -194,10 +194,12 @@ def xgb_classification_learner(df: pd.DataFrame,
             if params["objective"] == "multi:softprob":
                 shap_values_multiclass = {f"shap_values_{class_index}": list(value)
                                           for (class_index, value) in enumerate(shap_values)}
-                shap_expected_value_multiclass = {f"shap_expected_value_{class_index}":
-                                                      np.repeat(expected_value, len(class_shap_values))
-                                                  for (class_index, (expected_value, class_shap_values))
-                                                  in enumerate(zip(shap_expected_value, shap_values))}
+                shap_expected_value_multiclass = {
+                    f"shap_expected_value_{class_index}":
+                    np.repeat(expected_value, len(class_shap_values))
+                    for (class_index, (expected_value, class_shap_values))
+                    in enumerate(zip(shap_expected_value, shap_values))
+                }
                 shap_output = merge(shap_values_multiclass, shap_expected_value_multiclass)
 
             else:
@@ -553,9 +555,10 @@ def lgbm_classification_learner(df: pd.DataFrame,
                 shap_values_multiclass = {f"shap_values_{class_index}": list(value)
                                           for (class_index, value) in enumerate(shap_values)}
                 shap_expected_value_multiclass = {f"shap_expected_value_{class_index}":
-                                                      np.repeat(expected_value, len(class_shap_values))
-                                                  for (class_index, (expected_value, class_shap_values))
-                                                  in enumerate(zip(shap_expected_value, shap_values))}
+                    np.repeat(expected_value, len(class_shap_values))
+                    for (class_index, (expected_value, class_shap_values))
+                    in enumerate(zip(shap_expected_value, shap_values))
+                }
                 shap_output = merge(shap_values_multiclass, shap_expected_value_multiclass)
 
             else:
