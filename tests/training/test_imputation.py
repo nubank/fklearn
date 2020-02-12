c6747fb5cd566c47cf481e2ca81ba25f73535656
Vitor Santa Rosa <vitor.rosa@nubank.com.br>
2020-02-12 12:03:02 -0300

    Split test_imputer_with_fill_value as new test

--- tests/training/test_imputation.py
+++ tests/training/test_imputation.py
@@ -6,32 +6,46 @@ from fklearn.training.imputation import imputer, placeholder_imputer
 def test_imputer():
     input_df = pd.DataFrame({
         'col1': [10, 13, 10],
-        'col2': [50, 100, None],
-        'col3': [None, None, None]
+        'col2': [50, 100, None]
     })
 
     input_df2 = pd.DataFrame({
         'col1': [10, None],
-        'col2': [None, 100],
-        'col3': [None, 100]
+        'col2': [None, 100]
     })
 
     expected1 = pd.DataFrame({
+        'col1': [10.0, 13.0, 10.0],
+        'col2': [50.0, 100.0, 75.0]
+    })
+
+    expected2 = pd.DataFrame({
+        'col1': [10, 11.0],
+        'col2': [75.0, 100]
+    })
+
+    pred_fn, data, log = imputer(input_df, ["col1", "col2"], "mean")
+
+    assert expected1.equals(data)
+    assert expected2.equals(pred_fn(input_df2))
+
+
+def test_imputer_with_fill_value():
+    input_df = pd.DataFrame({
+        'col1': [10, 13, 10],
+        'col2': [50, 100, None],
+        'col3': [None, None, None]
+    })
+
+    expected = pd.DataFrame({
         'col1': [10.0, 13.0, 10.0],
         'col2': [50.0, 100.0, 75.0],
         'col3': [0, 0, 0]
     })
 
-    expected2 = pd.DataFrame({
-        'col1': [10, 11.0],
-        'col2': [75.0, 100],
-        'col3': [0.0, 100],
-    })
-
     pred_fn, data, log = imputer(input_df, ["col1", "col2", "col3"], "mean", fill_value=0)
 
-    assert expected1.equals(data)
-    assert expected2.equals(pred_fn(input_df2))
+    assert expected.equals(data)
 
 
 def test_placeholder_imputer():
