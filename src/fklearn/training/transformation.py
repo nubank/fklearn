e4f8b3f84fad46eab6cc4482a7e764a11e0ebde9
Jessica De Sousa <39313139+jessicadesousa@users.noreply.github.com>
2020-06-17 16:42:19 -0300

    space_time_split_dataset slowness (#138)
    * Changing the nan column to not include the first one dropped
    
    * Changing numpy filtering for pandas difference

--- src/fklearn/training/transformation.py
+++ src/fklearn/training/transformation.py
@@ -696,6 +696,7 @@ def onehot_categorizer(df: pd.DataFrame,
     vec = {column: categ_getter(column) for column in sorted(columns_to_categorize)}
 
     def p(new_df: pd.DataFrame) -> pd.DataFrame:
+
         make_dummies = lambda col: dict(map(lambda categ: ("fklearn_feat__" + col + "==" + str(categ),
                                                            (new_df[col] == categ).astype(int)),
                                             vec[col]))
