d922522a4332a862b8c54db3ac533b577b7f6066
silvanofilhonu <51379539+silvanofilhonu@users.noreply.github.com>
2020-07-07 19:11:15 -0300

    fix_tests_3

--- tests/training/test_calibration.py
+++ tests/training/test_calibration.py
@@ -32,24 +32,21 @@ def test_isotonic_calibration_learner():
 
 
 def test_find_thresholds_with_same_risk():
-    df_with_ecdf = pd.DataFrame([["group_1", 0.0, 416],
-                                 ["group_2", 0.0, 328],
-                                 ["group_2", 0.0, 670],
-                                 ["group_2", 0.0, 105],
-                                 ["group_2", 0.0, 427],
-                                 ["group_1", 1.0, 672],
-                                 ["group_1", 0.0, 635],
-                                 [None, 0.0, 158],
-                                 ["group_2", 0.0, 152],
-                                 ["group_1", 0.0, 305]],
+    df_with_ecdf = pd.DataFrame([["group_1", 0.0, 416, 3],
+                                 ["group_2", 0.0, 328, 2],
+                                 ["group_2", 0.0, 670, 4],
+                                 ["group_2", 0.0, 105, 1],
+                                 ["group_2", 0.0, 427, 3],
+                                 ["group_1", 1.0, 672, 4],
+                                 ["group_1", 0.0, 635, 4],
+                                 [None, 0.0, 158, 1],
+                                 ["group_2", 0.0, 152, 4],
+                                 ["group_1", 0.0, 305, 2]],
                                 columns=["sensitive_factor", "target",
-                                         "prediction_ecdf"])
-
-    band_size = 10
-    df_with_ecdf["unfair_band"] = np.floor(df_with_ecdf["prediction_ecdf"] / band_size) * band_size
+                                         "prediction_ecdf", "unfair_band"])
 
     df_expected = df_with_ecdf.copy()
-    df_expected["fair"] = pd.Series([10, 20, 40, 0, 30, 30, 20, None, 10, 0])
+    df_expected["fair"] = pd.Series([2, 3, 4, 1, 4, 4, 3, None, 2, 1])
     fair_thresholds = {'group_2': [-1, 105.0, 152.0, 328.0, 427.0, 670.0],
                        'group_1': [-1, 305.0, 416.0, 635.0, 672.0]}
 
