4993594f8273bf039cb0071762de58f69a6174bf
Caique Lima <caique.lima@nubank.com.br>
2020-09-28 17:53:24 -0300

    Add learner to return a fair calibration (#140)
    * Add find_thresholds_with_same_risk learner
    
    * change output column name
    
    * receive pre-ready bands
    
    * make output band_names similar to the pre-ready bands
    
    * make output_column -> output_column_name
    
    * return the output column only
    
    * removing unused import
    
    * fix_test
    
    * fix test 2
    
    * fix_tests_3
    
    * fix_test
    
    * remove unused import
    
    * correcting test fair thresholds
    
    * comparing dataframes instead of series
    
    * make function work on df copy
    
    * guarantee the output type is float
    
    * Rename prediction_ecdf parameter
    
    * fix formatting
    
    * Manually set parso lib range
    
    Co-authored-by: silvanofilhonu <51379539+silvanofilhonu@users.noreply.github.com>

--- src/fklearn/training/calibration.py
+++ src/fklearn/training/calibration.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sklearn
+
 from sklearn.isotonic import IsotonicRegression
 from toolz import curry
 
@@ -67,3 +68,99 @@ def isotonic_calibration_learner(df: pd.DataFrame,
 
 
 isotonic_calibration_learner.__doc__ += learner_return_docstring("Isotonic Calibration")
+
+
+@curry
+@log_learner_time(learner_name='find_thresholds_with_same_risk')
+def find_thresholds_with_same_risk(df: pd.DataFrame,
+                                   sensitive_factor: str,
+                                   unfair_band_column: str,
+                                   model_prediction_output: str,
+                                   target_column: str = "target",
+                                   output_column_name: str = "fair_band") -> LearnerReturnType:
+    """
+    Calculate fair calibration, where for each band any sensitive factor group have the same target mean.
+
+    Parameters
+    ----------
+
+    df : pandas.DataFrame
+        A Pandas' DataFrame with features and target columns.
+        The model will be trained to predict the target column
+        from the features.
+
+    sensitive_factor: str
+        Column where we have the different group classifications that we want to have the same target mean
+
+    unfair_band_column: str
+        Column with the original bands
+
+    model_prediction_output : str
+        Risk model's output
+
+    target_column : str
+        The name of the column in `df` that should be used as target for the model.
+        This column should be binary, since this is a classification model.
+
+    output_column_name : str
+        The name of the column with the fair bins.
+    """
+    sorted_df = df.sort_values(by=model_prediction_output).reset_index(drop=True)
+
+    def _find_thresholds_with_same_risk(df: pd.DataFrame,
+                                        metric_by_band: pd.DataFrame) -> list:
+        current_threshold = -1
+        fair_thresholds = [current_threshold]
+
+        for band, metric in metric_by_band.iterrows():
+            df = df[df[model_prediction_output] > current_threshold]
+            if df.empty:
+                break
+            df["cumulative_risk"] = df[target_column].expanding(min_periods=1).mean()
+            df["distance"] = abs(df["cumulative_risk"] - metric[target_column])
+            threshold = df.sort_values(by="distance").iloc[0][model_prediction_output]
+
+            fair_thresholds.append(threshold)
+
+            current_threshold = threshold
+
+        fair_thresholds[-1] = df[model_prediction_output].max()
+
+        return fair_thresholds
+
+    unfair_bands = sorted(sorted_df[unfair_band_column].unique())
+    metric_by_band = sorted_df.groupby(unfair_band_column).agg({target_column: "mean"})
+    sensitive_groups = list(filter(lambda x: x, sorted_df[sensitive_factor].unique()))
+    fair_thresholds = {}
+
+    for group in sensitive_groups:
+        raw_ecdf_with_target = sorted_df[sorted_df[sensitive_factor] == group][[model_prediction_output, target_column]]
+        fair_thresholds[group] = _find_thresholds_with_same_risk(raw_ecdf_with_target,
+                                                                 metric_by_band)
+
+    def p(new_df: pd.DataFrame) -> pd.DataFrame:
+        new_df_copy = new_df.copy()
+        new_df_copy[output_column_name] = pd.Series(dtype='int')
+        for group in sensitive_groups:
+            group_filter = new_df_copy[sensitive_factor] == group
+            n_of_bands = len(fair_thresholds[group]) - 1
+            new_df_copy.loc[group_filter, output_column_name] = pd.cut(
+                new_df_copy.loc[group_filter, model_prediction_output],
+                bins=fair_thresholds[group],
+                labels=unfair_bands[:n_of_bands]).astype(float)
+        return new_df_copy[output_column_name]
+
+    p.__doc__ = learner_pred_fn_docstring("find_thresholds_with_same_risk")
+
+    log = {'find_thresholds_with_same_risk': {
+        'output_column': output_column_name,
+        'prediction_ecdf': model_prediction_output,
+        'target_column': target_column,
+        'unfair_band_column': unfair_band_column,
+        'sensitive_factor': sensitive_factor,
+        'fair_thresholds': fair_thresholds}}
+
+    return p, p(df), log
+
+
+find_thresholds_with_same_risk.__doc__ += learner_return_docstring("find_thresholds_with_same_risk")
--- tests/training/test_calibration.py
+++ tests/training/test_calibration.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from fklearn.training.calibration import isotonic_calibration_learner
+from fklearn.training.calibration import (isotonic_calibration_learner,
+                                          find_thresholds_with_same_risk)
 
 
 def test_isotonic_calibration_learner():
@@ -27,3 +28,32 @@ def test_isotonic_calibration_learner():
     assert pred_test.calibrated_prediction.max() <= 1
     assert pred_test.calibrated_prediction.min() >= 0
     assert pred_test.calibrated_prediction.isnull().max() == 0
+
+
+def test_find_thresholds_with_same_risk():
+    df_with_ecdf = pd.DataFrame([["group_1", 0.0, 416, 3],
+                                 ["group_2", 0.0, 328, 2],
+                                 ["group_2", 0.0, 670, 4],
+                                 ["group_2", 0.0, 105, 1],
+                                 ["group_2", 0.0, 427, 3],
+                                 ["group_1", 1.0, 672, 4],
+                                 ["group_1", 0.0, 635, 4],
+                                 [None, 0.0, 158, 1],
+                                 ["group_2", 0.0, 152, 4],
+                                 ["group_1", 0.0, 305, 2]],
+                                columns=["sensitive_factor", "target",
+                                         "prediction_ecdf", "unfair_band"])
+
+    df_expected = df_with_ecdf.copy()
+    df_expected["fair"] = pd.Series([2, 3, 4, 1, 4, 4, 3, None, 2, 1])
+    fair_thresholds = {'group_2': [-1, 105.0, 152.0, 328.0, 670.0],
+                       'group_1': [-1, 305.0, 416.0, 635.0, 672.0]}
+
+    learner = find_thresholds_with_same_risk(sensitive_factor="sensitive_factor", unfair_band_column="unfair_band",
+                                             model_prediction_output="prediction_ecdf")
+
+    predict_fn, pred_df, log = learner(df_with_ecdf)
+    df_with_ecdf["fair"] = pred_df
+
+    assert fair_thresholds == log["find_thresholds_with_same_risk"]["fair_thresholds"]
+    pd.util.testing.assert_frame_equal(df_expected, df_with_ecdf)
