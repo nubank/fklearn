9bcbe724068db2e7e3f51fe5e7d50f6b8a2a2c14
Guilherme Beltramini <guilherme.beltramini@nubank.com.br>
2021-01-26 21:10:16 -0300

    Update 'setuptools' in venv

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+cur_dir=$(dirname ${BASH_SOURCE[0]})
+source $cur_dir/helpers.sh
+
 /usr/local/bin/python3 -m venv venv
+
+activate_venv
+
+venv/bin/python3 -m pip install -q 'setuptools==51.3.3'
