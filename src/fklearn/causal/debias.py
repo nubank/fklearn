f3a59842d29afc31365f7e9eee72961d0681d5ac
Matheus <matheusfacure01@gmail.com>
2021-10-19 18:28:56 -0300

    add debias with double ml

--- src/fklearn/causal/debias.py
+++ src/fklearn/causal/debias.py
@@ -1,9 +1,14 @@
-from statsmodels.formula.api import ols
-import pandas as pd
-from toolz import curry, merge
 from typing import List
 
+import numpy as np
+import pandas as pd
+from sklearn.base import RegressorMixin
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_predict
+from statsmodels.formula.api import ols
+from toolz import curry, merge
+
 
 @curry
 def debias_with_regression_formula(df: pd.DataFrame,
@@ -113,3 +118,75 @@ def debias_with_regression(df: pd.DataFrame,
     debiased = (df[cols_to_debias] - model.predict(df[confounders]) + df[cols_to_debias].mean())
 
     return df.assign(**{c + suffix: debiased[c] for c in cols_to_debias})
+
+
+@curry
+def debias_with_double_ml(df: pd.DataFrame,
+                          treatment: str,
+                          outcome: str,
+                          confounders: List[str],
+                          ml_regressor: RegressorMixin = GradientBoostingRegressor,
+                          hyperparam: dict = dict(max_depth=5),
+                          cv: int = 5,
+                          suffix: str = "_debiased",
+                          denoise: bool = True,
+                          seed: int = 123) -> pd.DataFrame:
+    """
+    Frisch-Waugh-Lovell style debiasing with ML model.
+    To debias, we
+     1) fit a regression ml model to predict the treatment from the confounders and take out of fold residuals from
+      this fit (debias step)
+     2) fit a regression ml model to predict the outcome from the confounders and take the out of fold residuals from
+      this fit (denoise step).
+    We then add back the average outcome and treatment so that their levels remain unchanged.
+
+    Returns a dataframe with the debiased columns with suffix appended to the name
+
+    Parameters
+    ----------
+
+    df : Pandas DataFrame
+        A Pandas' DataFrame with with treatment, an outcome and confounder columns
+
+    treatment : String
+        The name of the column in `df` with the treatment.
+
+    outcome : String
+        The name of the column in `df` with the outcome.
+
+    confounders : List of String
+        A list of confounder present in df
+
+    ml_regressor : Sklearn's RegressorMixin
+        A regressor model that implements a fit and a predict method
+
+    hyperparam : dict
+        The hyper-parameters for the model
+
+    cv : int
+        The number of folds to cross predict
+
+    suffix : String
+        A suffix to append to the returning debiased column names.
+
+    denoise : Bool (Default=True)
+        If it should denoise the outcome using the confounders or not
+
+    seed : int
+        A seed for consistency in random computation
+
+    Returns
+    ----------
+    debiased_df : Pandas DataFrame
+        The original `df` dataframe with debiased columns added.
+    """
+
+    cols_to_debias = [treatment, outcome] if denoise else [treatment]
+
+    np.random.seed(seed)
+    def get_cv_resid(col_to_debias: str) -> np.ndarray:
+        model = ml_regressor(**hyperparam)
+        cv_pred = cross_val_predict(estimator=model, X=df[confounders], y=df[col_to_debias], cv=cv)
+        return df[col_to_debias] - cv_pred + df[col_to_debias].mean()
+
+    return df.assign(**{c + suffix: get_cv_resid(c) for c in cols_to_debias})
--- tests/causal/debias_test.py
+++ tests/causal/debias_test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from fklearn.causal.debias import debias_with_regression_formula, debias_with_regression
+from fklearn.causal.debias import debias_with_regression_formula, debias_with_regression, debias_with_double_ml
 
 
 def test_debias_with_regression_formula():
@@ -21,7 +21,7 @@ def test_debias_with_regression_formula():
 
     pd.testing.assert_frame_equal(expected, result)
 
-    result2 = debias_with_regression_formula(df, "t", "y", "C(x)", suffix="_d", denoise=False)
+    result2 = debias_with_regression_formula(df, "t", "y", "C(x)", suffix="_d", denoise=False).round(3)
 
     pd.testing.assert_frame_equal(expected.drop(columns=["y_d"]), result2)
 
@@ -45,6 +45,30 @@ def test_debias_with_regression():
 
     pd.testing.assert_frame_equal(expected, result)
 
-    result2 = debias_with_regression(df, "t", "y", ["x"], suffix="_d", denoise=False)
+    result2 = debias_with_regression(df, "t", "y", ["x"], suffix="_d", denoise=False).round(3)
+
+    pd.testing.assert_frame_equal(expected.drop(columns=["y_d"]), result2)
+
+
+def test_debias_with_double_ml():
+    df = pd.DataFrame(dict(
+        x=[1, 1, 1, 2, 2, 2, 3, 3, 3],  # confounder
+        t=[3, 3, 4, 2, 2, 3, 1, 1, 2],
+        y=[1, 1, 2, 2, 2, 3, 2, 2, 3],
+    ))
+
+    result = debias_with_double_ml(df, "t", "y", ["x"], suffix="_d").round(3)
+
+    expected = pd.DataFrame(dict(
+        x=[1, 1, 1, 2, 2, 2, 3, 3, 3],
+        t=[3, 3, 4, 2, 2, 3, 1, 1, 2],
+        y=[1, 1, 2, 2, 2, 3, 2, 2, 3],
+        t_d=[1.333, 1.333, 3.333, 1.833, 2.333, 3.333, 1.333, 1.333, 3.333],
+        y_d=[1.0, 1.0, 3.0, 1.5, 2.0, 3.0, 1.0, 1.0, 3.0],
+    ))
+
+    pd.testing.assert_frame_equal(expected, result)
+
+    result2 = debias_with_double_ml(df, "t", "y", ["x"], suffix="_d", denoise=False).round(3)
 
     pd.testing.assert_frame_equal(expected.drop(columns=["y_d"]), result2)
