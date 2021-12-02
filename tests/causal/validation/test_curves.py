1195b165c85187e51ed9c91af2c600075233024d
Pedro Igor Oliveira <pedroigorao@hotmail.com>
2021-12-02 09:43:23 -0300

    add unit tests

--- tests/causal/validation/test_curves.py
+++ tests/causal/validation/test_curves.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+from pandas.util.testing import assert_frame_equal
 
 from fklearn.causal.effects import linear_effect
 from fklearn.causal.validation.curves import (effect_by_segment, cumulative_effect_curve, cumulative_gain_curve,
-                                              relative_cumulative_gain_curve)
+                                              relative_cumulative_gain_curve, effect_curves)
 
 
 def test_effect_by_segment():
@@ -66,3 +67,26 @@ def test_relative_cumulative_gain_curve():
                                             steps=df.shape[0], effect_fn=linear_effect)
 
     np.testing.assert_allclose(expected, result, rtol=1e-07)
+
+
+def test_effect_curves():
+
+    df = pd.DataFrame(dict(
+        t=[1, 1, 1, 2, 2, 2, 3, 3, 3],
+        x=[1, 2, 3, 1, 2, 3, 1, 2, 3],
+        y=[1, 1, 1, 2, 3, 4, 3, 5, 7],
+    ))
+
+    expected = pd.DataFrame({
+        "samples_count": [3, 4, 5, 6, 7, 8, 9],
+        "samples_fraction": [0.3333333, 0.4444444, 0.5555555, 0.6666666, 0.7777777, 0.8888888, 1.],
+        "cumulative_effect_curve": [3., 3., 2.92857143, 2.5, 2.5, 2.46153846, 2.],
+        "cumulative_gain_curve": [1., 1.33333333, 1.62698413, 1.66666667, 1.94444444, 2.18803419, 2.],
+        "relative_cumulative_gain_curve": [0.33333333, 0.44444444, 0.51587302, 0.33333333, 0.38888889, 0.41025641, 0.],
+        "random_model_cumulative_gain_curve": [0.6666666, 0.8888888, 1.1111111, 1.3333333, 1.5555555, 1.7777777, 2.],
+    })
+
+    result = effect_curves(df, prediction="x", outcome="y", treatment="t", min_rows=3, steps=df.shape[0],
+                           effect_fn=linear_effect)
+
+    assert_frame_equal(result, expected, rtol=1e-07)
