a7f1e46d10ef521c82d702a562bfe8cc9cdae141
Guilherme Beltramini <guilherme.beltramini@nubank.com.br>
2022-01-29 10:54:53 -0300

    Debug error in test [REVERT ME]

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,6 +7,6 @@ source $cur_dir/helpers.sh
 
 activate_venv
 
-venv/bin/python3 -m pytest --cov=fklearn tests/
+venv/bin/python3 -m pytest --cov=fklearn tests/training/test_transformation.py::test_custom_transformer
 
 codecov
