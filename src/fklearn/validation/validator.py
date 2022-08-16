63ce3d0842254759b156c83f846d4d27e78227ec
Nicolas Behar <nicolas.behar@nubank.com.br>
2022-08-16 15:44:26 -0300

    defined type to clean log parameters

--- src/fklearn/validation/validator.py
+++ src/fklearn/validation/validator.py
@@ -177,7 +177,7 @@ def validator(train_data: pd.DataFrame,
                        map(fold_iter),
                        partial(zip, logs))
 
-    def clean_logs(log_tuple) -> Tuple:
+    def clean_logs(log_tuple: Tuple[LogType, LogType]) -> Tuple[LogType, LogType]:
         split_log_error = list()
 
         new_validator_logs = list()
