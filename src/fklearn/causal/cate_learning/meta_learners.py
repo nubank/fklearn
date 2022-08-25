84753b058d5bd57585de41af07c187aa4ea8856b
Nicolas Behar <nicolas.behar@nubank.com.br>
2022-08-25 17:53:47 -0300

    fix missing newline

--- src/fklearn/causal/cate_learning/meta_learners.py
+++ src/fklearn/causal/cate_learning/meta_learners.py
@@ -186,8 +186,10 @@ def causal_s_classification_learner(
     is defined as τ(xi) = M(X=xi, T=1) - M(X=xi, T=0), being M a Machine Learning
     Model.
 
-    References:
+    **References:**
+
     [1] https://matheusfacure.github.io/python-causality-handbook/21-Meta-Learners.html
+
     [2] https://causalml.readthedocs.io/en/latest/methodology.html
 
     Parameters
