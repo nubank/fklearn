b97f331349847dba16d601c1d937f15a9eded067
Giulio Santo <giulio.santo@nubank.com.br>
2022-09-06 09:55:48 -0300

    Included return type

--- src/fklearn/exceptions/exceptions.py
+++ src/fklearn/exceptions/exceptions.py
@@ -1,14 +1,14 @@
 class MultipleTreatmentsError(Exception):
     def __init__(
         self, msg: str = "Data contains multiple treatments.", *args, **kwargs
-    ):
+    ) -> None:
         super().__init__(msg, *args, **kwargs)
 
 
 class MissingControlError(Exception):
     def __init__(
         self, msg: str = "Data does not contain the specified control.", *args, **kwargs
-    ):
+    ) -> None:
         super().__init__(msg, *args, **kwargs)
 
 
@@ -18,5 +18,5 @@ class MissingTreatmentError(Exception):
         msg: str = "Data does not contain the specified treatment.",
         *args,
         **kwargs
-    ):
+    ) -> None:
         super().__init__(msg, *args, **kwargs)
