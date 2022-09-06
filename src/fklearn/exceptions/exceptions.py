5b5ea92be13045bd1e581d3c1d78b8c4372f33a3
Nicolas Behar <nicolas.behar@nubank.com.br>
2022-09-06 09:42:12 -0300

    including missing type

--- src/fklearn/exceptions/exceptions.py
+++ src/fklearn/exceptions/exceptions.py
@@ -1,17 +1,17 @@
 class MultipleTreatmentsError(Exception):
-    def __init__(self, msg="Data contains multiple treatments.", *args, **kwargs):
+    def __init__(self, msg: str ="Data contains multiple treatments.", *args, **kwargs):
         super().__init__(msg, *args, **kwargs)
 
 
 class MissingControlError(Exception):
     def __init__(
-        self, msg="Data does not contain the specified control.", *args, **kwargs
+        self, msg: str ="Data does not contain the specified control.", *args, **kwargs
     ):
         super().__init__(msg, *args, **kwargs)
 
 
 class MissingTreatmentError(Exception):
     def __init__(
-        self, msg="Data does not contain the specified treatment.", *args, **kwargs
+        self, msg: str ="Data does not contain the specified treatment.", *args, **kwargs
     ):
         super().__init__(msg, *args, **kwargs)
