f94675aa28598d03156e49f7958ffc4d46c02a30
Hector Lira <hector.liratl@gmail.com>
2022-11-23 09:32:26 -0600

    Document quantile_partitioner function

--- src/fklearn/causal/partitioners.py
+++ src/fklearn/causal/partitioners.py
@@ -4,7 +4,24 @@ from toolz import curry
 
 
 @curry
-def quantile_partitioner(series: pd.Series, segments: int = 10) -> List:
+def quantile_partitioner(series: pd.Series, segments: int) -> List:
+    """
+    Returns the bins using a quantile-based approach.
+
+    Parameters
+    ----------
+    series : Pandas' Series
+        A Pandas' Series to partition by quantiles.
+
+    segments : int
+        The number of bins to be computed.
+
+
+    Returns
+    ----------
+    bins: Array
+        The bins to partition the series argument.
+    """
 
     _, bins = pd.qcut(series, q=segments, retbins=True)
 
