913a9a8c371aaef0fed3de49897fbcb09fef1572
Flavio Pingas <fpingas@hotmail.com>
2023-03-24 13:10:12 +0100

    rollback changes to coveragerc; increment setup classifiers

--- setup.py
+++ setup.py
@@ -46,5 +46,9 @@ setup(name=MODULE_NAME,
                       "all": all_deps},
       include_package_data=True,
       zip_safe=False,
-      classifiers=['Programming Language :: Python :: 3.6'])
-
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9'
+          ])
