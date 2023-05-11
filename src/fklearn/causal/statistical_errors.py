3db1f327cf4cab760ed7a6cbbcdef50414446774
Hector Lira <hector.liratl@gmail.com>
2023-05-11 09:50:20 -0600

    Fixes code style in statistical_errors.py

--- src/fklearn/causal/statistical_errors.py
+++ src/fklearn/causal/statistical_errors.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 from fklearn.causal.effects import linear_effect
 
+
 def linear_standard_error(df: pd.DataFrame, treatment: str, outcome: str) -> float:
     """
     Linear Standard Error
 
-    Returns a Float: the linear standard error of a linear regression 
+    Returns a Float: the linear standard error of a linear regression
     of the outcome as a function of the treatment.
 
     Parameters
@@ -24,9 +25,10 @@ def linear_standard_error(df: pd.DataFrame, treatment: str, outcome: str) -> flo
     Returns
     ----------
     se : Float
-        A Float of the linear standard error extracted by using the formula for 
+        A Float of the linear standard error extracted by using the formula for
         the simple linear regression.
     """
+
     n = df.shape[0]
     t_bar = df[treatment].mean()
     beta1 = linear_effect(df, treatment, outcome)
