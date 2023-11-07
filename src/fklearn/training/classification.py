760343ef3dc0ea789c4171b473ff7208c6e3cd24
Otavio Vasques <otavio.vasques@nubank.com.br>
2023-11-07 14:15:45 -0300

    Add typing extensions for python 3.7 support

--- src/fklearn/training/classification.py
+++ src/fklearn/training/classification.py
@@ -1,4 +1,5 @@
-from typing import List, Any, Optional, Callable, Tuple, Union, TYPE_CHECKING, Literal
+from typing import List, Any, Optional, Callable, Tuple, Union, TYPE_CHECKING
+from typing_extensions import Literal
 
 import numpy as np
 import pandas as pd
