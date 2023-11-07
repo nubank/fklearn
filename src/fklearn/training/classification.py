8dca30131a3e42624230b91331efd18ea6e60c82
Otavio Vasques <otavio.vasques@nubank.com.br>
2023-11-07 15:17:52 -0300

    Reduce type list

--- src/fklearn/training/classification.py
+++ src/fklearn/training/classification.py
@@ -537,7 +537,8 @@ def lgbm_classification_learner(
         categorical_feature: Union[List[str], List[int], Literal['auto']] = 'auto',
         keep_training_booster: bool = False,
         callbacks: Optional[List[Callable]] = None,
-        dataset_init_score: Optional[Union[List, List[List], np.ndarray, pd.Series, pd.DataFrame]] = None
+        #dataset_init_score: Optional[Union[List, List[List], np.ndarray, pd.Series, pd.DataFrame]] = None
+        dataset_init_score: Optional[Union[List]] = None
 ) -> LearnerReturnType:
     """
     Fits an LGBM classifier to the dataset.
